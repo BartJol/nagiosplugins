7f7cb2b97253470e67fab318a90b7e6d9ca9b290
BartJol <git@bartjol.nl>
2010-12-30 16:54:05 +0100

    deleted extra code

--- check_compare_files.pl
+++ check_compare_files.pl
@@ -1,22 +1,26 @@
 #!/data/wre/prereqs/bin/perl
 use strict;
 
-use constant NAGIOS_OK      => 0;
-use constant NAGIOS_WARN    => 1;
-use constant NAGIOS_CRIT    => 2;
-use constant NAGIOS_UNKNOWN => 3;
+#use constant NAGIOS_OK      => 0;
+#use constant NAGIOS_WARN    => 1;
+#use constant NAGIOS_CRIT    => 2;
+#use constant NAGIOS_UNKNOWN => 3;
+my %ERRORS=('OK'=>0,'WARNING'=>1,'CRITICAL'=>2,'UNKNOWN'=>3,'DEPENDENT'=>4);
 use DBI;
 
 my $location1       = @ARGV[0];
 my $location2       = @ARGV[1];
-my $baselineFile    = @ARGV[2];
-my $impact          = @ARGV[3] || "0";
-#my $nagiosTmpDir = 'http://nagios.bartjol.nl/tmp/';
+my $baselineFile    = @ARGV[2] || "";
+my $impact          = @ARGV[3] || "warn";
+
+print $impact . "\n";
 
 if (!$location1 || !$location2 || !$baselineFile) {
 	print "Not all locations needed for this check are configured\n";
-	exit(NAGIOS_WARN);
+	exit $ERRORS{"WARNING"};
 }
+
+open(baseline, $baselineFile) or die "An existing baseline file is required";
 
 #make a dir with a randomized name to save files in
 my $subdir = getId();
@@ -42,23 +46,28 @@ system ("diff $file1 $file2 > $diffFile");
 my $compareResult = `diff $diffFile $baselineFile`;
 system ( "diff $diffFile $baselineFile > $diffFile2");
 
+$compareResult =~ s/\n/\<br \/\>/g;
+
 if (length($compareResult) eq "0") {
     print("Compared files are below thresshold differences");
-#    system ("rm -r $tmpDir");
-    exit(NAGIOS_OK);
+    system ("rm -r $tmpDir");
+    exit $ERRORS{"OK"};
 }
-elsif ($impact eq "0") {
+elsif ($impact eq "warn") {
     print("Compared files differ too much");
-#    system ("rm -r $tmpDir");
-    exit(NAGIOS_WARN)|$compareResult;
+    system ("rm -r $tmpDir");
+    (defined($compareResult)) ?  print " | ",$compareResult,"\n" : print "\n";
+     exit $ERRORS{"WARNING"};
+
 }
-elsif ($impact ne "0") {
+elsif ($impact eq "crit") {
     print("Compared files differ too much");
-#    system ("rm -r $tmpDir");
-    exit(NAGIOS_CRIT);
+    system ("rm -r $tmpDir");
+    (defined($compareResult)) ?  print " | ",$compareResult,"\n" : print "\n";
+    exit $ERRORS{"CRITICAL"};
 }
 else {
-    exit(NAGIOS_UNKNOWN);
+    exit $ERRORS {"UNKNOWN"};
 }
 
 # a sub to make a randomized dir to save files
